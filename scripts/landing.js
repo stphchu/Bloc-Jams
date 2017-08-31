1f6d7df49089bc7767902cd03cbe2705c5a8b09b
Stephanie Chu <esceeaitchyu@gmail.com>
2017-08-30 22:35:58 -0700

    Fix bug in the js file for the 'revealPoint' function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,11 @@
 var animatePoints = function() {
   var points = document.getElementsByClassName('point');
 
-  var revealPoint = function (pointsIndex){
-      pointsIndex[i].style.opacity = 1;
-      pointsIndex[i].style.transform = "scaleX(1) translateY(0)";
-      pointsIndex[i].style.msTransform = "scaleX(1) translateY(0)";
-      pointsIndex[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+  var revealPoint = function (index){
+      points[index].style.opacity = 1;
+      points[index].style.transform = "scaleX(1) translateY(0)";
+      points[index].style.msTransform = "scaleX(1) translateY(0)";
+      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 
 for (var i = 0; i < points.length; i++){
