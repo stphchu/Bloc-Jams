7a61c79fecd940c6c69c81f822410adb20cef3f6
Stephanie Chu <esceeaitchyu@gmail.com>
2017-09-02 18:39:40 -0700

    Add scroll animation event listener -- possible bug

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+var animatePoints = function(points) {
   var revealPoint = function (index){
       points[index].style.opacity = 1;
       points[index].style.transform = "scaleX(1) translateY(0)";
@@ -12,3 +12,10 @@ for (var i = 0; i < points.length; i++){
   revealPoint(i);
 };
 };
+
+window.onload = function() {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  window.addEventListener('scroll', function(event) {
+  console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+});
+}
