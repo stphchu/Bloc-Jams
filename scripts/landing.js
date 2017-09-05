deeccfb9e54e60de9db5c8af6c5338d7bab79cef
Stephanie Chu <esceeaitchyu@gmail.com>
2017-09-04 18:17:10 -0700

    Add scroll animation to landing.js (fixed bug); add 'forEach' function in utilites.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,15 +7,25 @@ var animatePoints = function(points) {
       points[index].style.msTransform = "scaleX(1) translateY(0)";
       points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-
+/*
 for (var i = 0; i < points.length; i++){
   revealPoint(i);
 };
+*/
+
+forEach(pointsArray, revealPoint);
 };
 
 window.onload = function() {
+// Automatically animate the points on a tall screen where scrolling can't trigger the animation
+ if (window.innerHeight > 950) {
+     animatePoints(pointsArray);
+ }
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
   window.addEventListener('scroll', function(event) {
-  console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+    }
 });
 }
