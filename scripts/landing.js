25ce0bc24035445b8b7d2b06a451c7dd3997bae5
Stephanie Chu <esceeaitchyu@gmail.com>
2017-09-20 22:30:08 -0700

    Refactor animation functions to use jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,15 @@
-var pointsArray = document.getElementsByClassName('point');
+// #5
+var animatePoints = function() {
+  var revealPoint = function() {
+// #7
+   $(this).css({
+   opacity: 1,
+   transform: 'scaleX(1) translateY(0)'
+   });
+  };
+// #6
+   $.each($('.point'), revealPoint);
 
-var animatePoints = function(points) {
-  var revealPoint = function (index){
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
 /*
 for (var i = 0; i < points.length; i++){
   revealPoint(i);
@@ -16,16 +19,19 @@ for (var i = 0; i < points.length; i++){
 forEach(pointsArray, revealPoint);
 };
 
-window.onload = function() {
+$(window).load(function() {
 // Automatically animate the points on a tall screen where scrolling can't trigger the animation
- if (window.innerHeight > 950) {
-     animatePoints(pointsArray);
- }
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-  window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);
-    }
+// #1
+  if ($(window).height() > 950) {
+    animatePoints();
+  }
+// #2
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+// #3
+  $(window).scroll(function(event) {
+// #4
+  if ($(window).scrollTop() >= scrollDistance) {
+    animatePoints();
+  }
+  });
 });
-}
