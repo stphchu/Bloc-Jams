7a05530253ccfc34443cb4cd8c4839462f3e8f91
Stephanie Chu <esceeaitchyu@gmail.com>
2017-09-26 21:42:14 -0700

    Refactor JS code with jQuery equivalent

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+// #1
+var buildCollectionItemTemplate = function() {
+  var template =
     '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
@@ -14,14 +16,19 @@ var collectionItemTemplate =
   + '</div>'
   ;
 
-  window.onload = function() {
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
+  // #2
+  return $(template);
+};
 
-    // #3
+ $(window).load(function() {
+  // #3
+   var $collectionContainer = $('.album-covers');
+  // #4
+   $collectionContainer.empty();
+
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+      var $newThumbnail = buildCollectionItemTemplate();
+  // #5
+       $collectionContainer.append($newThumbnail);
     }
-}
+ });
